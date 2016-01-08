4e2dbaa226796eb9d66446df04d068b2b37aaeae
Joel Welling <welling@psc.edu>
2016-01-08 02:04:20 -0500

    First version.

---
+++ src/helloworld/helloworld.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+'''
+Created on Jan 8, 2016
+
+@author: welling
+'''
+
+
+def main():
+    print """
+    Hello World, from...
+    Joel Welling
+    """
+
+
+if __name__ == '__main__':
+    main()
